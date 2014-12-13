9b119213e4fec94d65bb57d670396cdea7704582
Christopher Fonseka <chris@roli.com>
2014-12-13 17:54:31 +0000

    Initial Commit

---
+++ SeaboardBattleBreakout/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SeaboardBattleBreakout
+//
+//  Created by Christopher Fonseka on 13/12/2014.
+//  Copyright (c) 2014 ROLI. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
